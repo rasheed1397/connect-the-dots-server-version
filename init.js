3559c5d72d718715f9028a7388f5e628e2f430aa
Ricardo O. Mitchell <rasheed1397@gmail.com>
2020-06-09 17:00:46 -0400

    server.js updated to handle ui request & del files

--- init.js
+++ init.js
@@ -5,8 +5,8 @@ const node = document.getElementById('app');
 // Update the second argument to `Elm.Main.embed` with your selected API.
 // See `doc/intro.md` for more information.
 const app = Elm.Main.embed(node, {
-    api: '',
-    hostname: '',
+    api: 'Http',
+    hostname: 'http://localhost:8080',
 });
 
 app.ports.startTimer.subscribe((int) => {
--- server.js
+++ server.js
@@ -1,30 +1,152 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var gameLogic = require('./game-logic');
 var app = express();
+const gridCount = 4;
+let firstMoveDone = false;
+let startNodeRegistered = false;
+let GAME_OVER = false;
+global.endPoints = []; //for tracking the points where a new line can begin
+let currentPlayer = 1;
+let startNodePoint = {};
+global.gameMovePoints = []; //updated for every valid complete move and holds all the nodes of line
+global.gameDiagonalLines = []; // all diagonal lines for checking line crosses at non-node points
+let response = {};
+
+let INITIALIZE = {
+    "msg": "INITIALIZE",
+    "body": {
+        "newLine": null,
+        "heading": "Player 1",
+        "message": "Awaiting Player 1's Move"
+    }
+};
+let VALID_START_NODE = {
+    "msg": "VALID_START_NODE",
+    "body": {
+        "newLine": null,
+        "heading": "Player  ", //add player later
+        "message": "Select a second node to complete the line."
+    }
+};
+let INVALID_START_NODE = {
+    "msg": "INVALID_START_NODE",
+    "body": {
+        "newLine": null,
+        "heading": "Player ", //add player later
+        "message": "Not a valid starting position."
+    }
+};
+let VALID_END_NODE = {
+    "msg": "VALID_END_NODE",
+    "body": {
+        "newLine": {
+            "start": {}, //add start later
+            "end": {}    //add end later
+        },
+        "heading": "Player ",
+        "message": null
+    }
+};
+let INVALID_END_NODE = {
+    "msg": "INVALID_END_NODE",
+    "body": {
+        "newLine": null,
+        "heading": "Player ", //add player later
+        "message": "Invalid move!"
+    }
+};
+let UPDATED_TEXT = {
+    "msg": "UPDATE_TEXT",
+    "body": {
+        "newLine": null,
+        "heading": "Player ", //add player later
+        "message": "Are you asleep?"
+    }
+};
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.options('/node-clicked', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    res.send(204);
+});
 
 app.get('/initialize', function (req, res) {
-    var response = {
-        "msg": "INITIALIZE",
-        "body": {
-            "newLine": null,
-            "heading": "Player 1",
-            "message": "Awaiting Player 1's Move"
-        }
-    };
+    res.header("Access-Control-Allow-Origin", "*");
+    response = JSON.parse(JSON.stringify(INITIALIZE));
+    resetGame();
     res.send(response);
     console.log(response);
-  });
-
-app.post('/', function (req, res) {
-    var output = req.body;
-    res.send(output);
-    console.log(output);
-  });
-
-app.listen(8080, function()
-{
-    console.log("Allahu Akbar");
 });
+app.post('/node-clicked', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    if (!GAME_OVER) //do nothing if game is over
+    {
+        if (!startNodeRegistered && !firstMoveDone) //1st move start point
+        {
+            response = JSON.parse(JSON.stringify(VALID_START_NODE));
+            response.body.heading += currentPlayer;
+            startNodeRegistered = true;
+            startNodePoint = JSON.parse(JSON.stringify(req.body));
+        }
+        else if (!startNodeRegistered && firstMoveDone) //not the 1st move so check the new point matches either of the current end points
+        {
+            if (gameLogic.validStartNode(req.body, endPoints)) {
+                startNodeRegistered = true;
+                startNodePoint = JSON.parse(JSON.stringify(req.body));
+                response = JSON.parse(JSON.stringify(VALID_START_NODE));
+                response.body.heading += currentPlayer;
+            }
+            else {
+                response = JSON.parse(JSON.stringify(INVALID_START_NODE));
+                response.body.heading += currentPlayer;
+            }
+        }
+        else if (startNodeRegistered) //1st point already registered, now check the end point.
+        {
+            response = JSON.parse(JSON.stringify(VALID_END_NODE));
+            response.body.newLine.start = JSON.parse(JSON.stringify(startNodePoint));
+            response.body.newLine.end = JSON.parse(JSON.stringify(req.body));
+            response.body.heading += currentPlayer;
+            currentPlayer = (currentPlayer % 2) + 1;
+            startNodeRegistered = false;
+
+            if (gameLogic.addAllPoints(startNodePoint, req.body, gameMovePoints, gameDiagonalLines) !== -1) // add all the points of the new line or report invalid line
+            {
+                firstMoveDone = true;
+
+                gameLogic.updateEndPoints(startNodePoint, req.body, endPoints);
+
+                if (gameLogic.isGameOver(endPoints[0], endPoints[1], gameMovePoints, gridCount)) {
+                    GAME_OVER = true;
+                    response.msg = "GAME_OVER";
+                    response.body.heading = "Game Over"
+                    response.body.message = "Player " + currentPlayer; //The other player won
+                }
+            }
+            else {
+                currentPlayer = (currentPlayer % 2) + 1; //refer back to the current player
+                response = JSON.parse(JSON.stringify(INVALID_END_NODE));
+                response.body.heading += currentPlayer;
+            }
+        }
+        res.send(response);
+    }
+});
+
+app.listen(8080, function () {
+    console.log("API Running");
+});
+
+function resetGame() {
+    GAME_OVER = false;
+    firstMoveDone = false;
+    currentPlayer = 1;
+    startNodePoint = {};
+    endPoints.length = 0;
+    gameMovePoints.length = 0;
+    gameDiagonalLines.length = 0;
+}
